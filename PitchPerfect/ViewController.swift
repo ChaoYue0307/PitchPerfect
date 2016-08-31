639efceade0555a353208f4dad7ccdd467f550cb
ChaoYue <hechaoyue0307@gmail.com>
2016-08-31 10:43:15 +0800

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by He Chaoyue on 8/31/16.
+//  Copyright © 2016 He Chaoyue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
